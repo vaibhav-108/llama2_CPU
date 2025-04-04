a252d9642a32d74e06be2267050625ef4e321200
vaibhav-108 <vaibhav.b108@gmail.com>
2025-04-04 12:43:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Vaibhav',
+    author_email= 'vaibhav.b108@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
